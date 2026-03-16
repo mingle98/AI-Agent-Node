4cf5f71ae29d2c1fc846c7f189624143981390d7
zhoumingle <2293188960@qq.com>
2026-03-16 20:05:32 +0800

    调试支持

--- server.js
+++ server.js
@@ -66,6 +66,7 @@ async function getAgent() {
 const app = express();
 
 app.use(express.json({ limit: "1mb" }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", CORS_ORIGIN);
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
